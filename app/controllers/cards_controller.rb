24c8962fe6930fb152f2ca2b61c50b16440dd664
Sergey <adress_2009@mail.ru>
2017-10-24 23:32:59 +0300

    fourth task compleate

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,4 +3,48 @@ class CardsController < ApplicationController
     	@cards = Card.all
     end
 
+    def show
+    	@card = Card.find(params[:id])
+  	end
+
+  	def new
+  		@card = Card.new(:review_date => (Date.today+3.day).to_date)
+  	end
+
+  	def edit
+  		@card = Card.find(params[:id])
+	end
+
+  	def create
+  		@card = Card.new(card_params)
+ 
+  		if @card.save
+    		redirect_to @card
+  		else
+    		render 'new'
+  		end
+	end
+
+	def update
+  		@card = Card.find(params[:id])
+ 
+  		if @card.update(card_params)
+    		redirect_to @card
+  		else
+    		render 'edit'
+  		end
+	end
+
+	def destroy
+    	@card = Card.find(params[:id])
+    	@card.destroy
+ 
+    	redirect_to cards_path
+  	end
+
+  	private
+  		def card_params
+    		params.require(:card).permit(:original_text, :translated_text, :review_date)
+  		end
 end
+
